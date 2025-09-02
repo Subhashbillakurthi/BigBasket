413b84b64e41569c78af11eb459bcc1967aac4e9
Subhash Billakurthi <rrsubhash342002@gmail.com>
2025-09-02 20:20:41 +0530

    login changes

--- core/urls.py
+++ core/urls.py
@@ -20,7 +20,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('accounts/', include('accounts.urls')),
+    path('', include('accounts.urls')),
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
     path('orders/', include('orders.urls')),
